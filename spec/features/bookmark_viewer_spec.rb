6c21e28d0126a78761af7092e208f14b5f04d777
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-05-12 11:50:38 +0100

    refactored viewer spec

--- spec/features/bookmark_viewer_spec.rb
+++ spec/features/bookmark_viewer_spec.rb
@@ -2,11 +2,10 @@ require 'pg'
 
 feature 'displays list of bookmarks' do
   scenario 'user sees a list of bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    connection.exec("INSERT INTO bookmarks VALUES(1,'http://www.makersacademy.com/');")
-    connection.exec("INSERT INTO bookmarks VALUES(2,'http://www.destroyallsoftware.com/');")
-    connection.exec("INSERT INTO bookmarks VALUES(3,'http://www.google.com/');")
+    Bookmark_Library.create(url: 'http://www.makersacademy.com/')
+    Bookmark_Library.create(url: 'http://www.destroyallsoftware.com/')
+    Bookmark_Library.create(url: 'http://www.google.com/')
 
     visit '/bookmarks'
     expect(has_expected_content(page)).to be true
